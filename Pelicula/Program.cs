a0408bf326bd212807fc6d317bd84dafccdc39f3
KARIME ISABEL MORA GUTIERREZ <60378108+KarimeIsabel@users.noreply.github.com>
2020-02-22 23:14:31 -0800

    atributos privados

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -4,31 +4,46 @@ namespace Peliculas
 {
    class Pelicula
     {
-        public  string  titulo;
-        public Int16 año;
-        public string pais;
-        public string director;
+         private string Titulo;
+         private int Año;
+         private string Pais;
+         private string Director;
+        //atributos
+        private  string  titulo;
+        private Int16 año;
+        private string pais;
+        private string director;
+        
+         //Metodos
+        public string gettitulo()
+        {
+            return titulo;
+        }
+        public void settitulo(string Titulo)
+        {
+            titulo=Titulo;
+        }
+        public Int16 getaño() 
+        {
+            return año;
+        }
+        public void setaño(Int16 Año)
+        {
+            año=Año;
+        }
     }
-
-
     class Program
     {
         static void Main(string[] args)
         {
            Pelicula p1= new Pelicula();
-           p1.titulo="El viaje de chijiro";
-           p1.año=2001;
-
-           Pelicula p2= new Pelicula();
-           p2.titulo="Toy Story 4";
-           p2.año=2019;
-           
-
-// Impresiones
-System.Console.WriteLine("Titulo: "+p1.titulo+"\nAño: "+p1.año );
-System.Console.WriteLine("Titulo: "+p2.titulo+"\nAño: "+p2.año );
-
-
+           p1.settitulo("El viaje de chijiro");
+           p1.setaño(2001);
+           Pelicula p2=new Pelicula();
+           p2.settitulo("Toy Story 4");
+           p2.setaño(2019);
+           System.Console.WriteLine("Titulo: "+p1.gettitulo()+"\nAño: "+p1.getaño());
+           System.Console.WriteLine("Titulo: "+p2.gettitulo()+"\nAño: "+p2.getaño());
         }
     }
 }
