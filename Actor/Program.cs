1975c0fe072a9e04c9e013279ba6c8325b2d704f
KARIME ISABEL MORA GUTIERREZ <60378108+KarimeIsabel@users.noreply.github.com>
2020-03-09 21:13:10 -0700

    Lista de actores

--- Actor/Program.cs
+++ Actor/Program.cs
@@ -3,19 +3,78 @@ using System.Collections.Generic;
 
 namespace ListPeliculas
 {
+    public class Actor
+    {
+        private string Nombre;
+        private int AñoNac;
+
+        public Actor(string Nombre, int AñoNac)
+        {
+            this.Nombre=Nombre;
+            this.AñoNac=AñoNac;
+        }
+
+        public void setNombre(string n)
+        {
+            this.Nombre=n;
+        }
+        public String getNombre()
+        {
+            return Nombre;
+        }
+
+        public void setAñoNac(int x)
+        {
+            this.AñoNac=x;
+        }
+        public int getAñoNac()
+        {
+            return AñoNac;
+        }
+    }
+
     public class Pelicula
     {
         private string Nombre;
         private Int16 Año;
+        private List<Actor>Actores;
         public Pelicula(string nombre, Int16 año)
         {
-            this.Nombre= nombre;
+            this.Nombre= Titulo;
             this.Año=año;
+            Actores=new List<Actor>();
+        }
+      public void setTitulo(string t)
+        {
+            this.Nombre=t;
+        }
+        public string gretTitulo()
+        {
+            return Nombre;
+        }
+
+        public void setAño (Int16 y)
+        {
+            this.Año=y;
+        }
+        public int getAño ()
+        {
+         return Año;
+        }
+
+        public void AgregarActor(Actor A)
+        {
+         Actores.Add(A);
         }
         public void imprime()
         {
             Console.WriteLine("Titulo: "+Nombre);
             Console.WriteLine("Año: "+ Año);
+            foreach(Actor a in Actores)
+            {
+                Console.WriteLine(a.getNombre());
+                Console.WriteLine(a.getAñoNac());
+            }
         }
     }
 
@@ -23,17 +82,12 @@ namespace ListPeliculas
     {
         static void Main(string[] args)
         {
-            List<Pelicula> p1=new List<Pelicula>();
-            p1.Add(new Pelicula("El viaje de chihiro",2001));
-            p1.Add(new Pelicula ("Toy Story 4",2019));
-            p1.Add(new Pelicula("La lista de Schindler",1993));
-            p1.Add(new Pelicula ("Gladiator",2000));
-            p1.Add(new Pelicula("El Señor de los Anillos: el retorno del Rey",2003));
+            Pelicula p1 = new Pelicula ("Historia de un matrimonio",2016);
+            p1.AgregarActor(new Actor("Scarlett Johansson",1984));
+            p1.AgregarActor(new Actor ("Adam Drive",1983));
+           
+            p1.imprime();
             
-            foreach(Pelicula año in p1)
-            {
-                año.imprime();
-            }
         }
     }
 }
